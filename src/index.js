ee88ec0a95a76b5a66a80577bcd0ac4391048687
irshadali740 <irshadali740@gmail.com>
2022-06-03 23:39:32 +0530

    mongoosepush

--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@ const express = require('express');
 var bodyParser = require('body-parser');
 
 const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+mongoose.connect("mongodb+srv://irshadali740:Pn0krp5ehwjTD6H1@cluster0.9m8b0.mongodb.net/irshad740-DB?retryWrites=true&w=majority", {
+      useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err))
+
 
 app.use('/', route);
 
