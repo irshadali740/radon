aae59bf83a00f86c503b4127b2c2f4c4915f41da
irshadali740 <irshadali740@gmail.com>
2022-06-05 14:54:52 +0530

    no new

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://irshadali740:WIlwCJ2wGIPgPgxD@cluster0.9m8b0.mongodb.net/irshad740-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
