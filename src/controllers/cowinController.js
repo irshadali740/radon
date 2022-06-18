f6e6e7074b69bf6ffd3d4a754acdbf9a605f0ab2
irshadali740 <irshadali740@gmail.com>
2022-06-18 22:33:32 +0530

    mems

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -97,6 +97,8 @@ let getBydistrict = async function (req, res) {
     }
 }
 
+
+
 let temperatureofLondon = async function (req, res) {
     try {
         let city = req.query.q
@@ -106,23 +108,60 @@ let temperatureofLondon = async function (req, res) {
             url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${app_id}`
         }
         let result = await axios(options)
-        res.status(200).send({ msg: "The temperature of London is" + " " + result.data.main.temp + " " + "in Farhenheit" })
+        res.status(200).send({ msg: "The temperature of London is" + " " + result.data.main.temp + " " + "in Kelvin" })
     }
     catch (err) {
         res.status(500).send({ msg: err.message })
     }
 }
 
-// let sortofcities = async function (req,res){
-//     try{
-//         let 
-//     }
-// }
 
-    
+
+let getsortedofcities = async function (req, res) {
+    try {
+        let cities = ["Bengaluru", "Mumbai", "Delhi", "Kolkata", "Moscow", "Chennai", "London"]
+        let cityObjArray = []
+        for (i = 0; i < cities.length; i++) {
+            let obj = { city: cities[i] }
+            let resp = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${cities[i]}&appid=5c73ea4995196e0c0b025e329c3ce7aa`)
+            obj.temp = resp.data.main.temp
+            cityObjArray.push(obj)
+        }
+        let sorted = cityObjArray.sort(function (a, b) { return a.temp - b.temp })
+        res.status(200).send({ status: true, data: sorted })
+    } catch (error) {
+        res.status(500).send({ status: false, msg: "server error" })
+    }
+
+}
+
+
+
+let meme = async function (req, res) {
+    try {
+        let memeid = req.query.template_id
+        let firsttext = req.query.text0
+        let secondtext = req.query.text1
+        let user = req.query.username
+        let password1 = req.query.password
+        var options = {
+            method: "post",
+            url: `https://api.imgflip.com/caption_image?template_id=${memeid}&text0=${firsttext}&text1=${secondtext}&username=${user}&password=${password1}`
+        }
+        let result = await axios(options)
+        res.status(200).send({ msg: result.data })
+    }
+    catch (err) {
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+
 module.exports.getStates = getStates
 module.exports.getDistricts = getDistricts
 module.exports.getByPin = getByPin
 module.exports.getOtp = getOtp
 module.exports.getBydistrict = getBydistrict
 module.exports.temperatureofLondon = temperatureofLondon
+module.exports.getsortedofcities = getsortedofcities
+module.exports.meme = meme
--- src/routes/route.js
+++ src/routes/route.js
@@ -9,13 +9,11 @@ router.get("/test-me", function (req, res) {
 })
 
 
-router.get("/cowin/states", CowinController.getStates)
-router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
-router.get("/cowin/getByPin", CowinController.getByPin)
-
 router.post("/cowin/getOtp", CowinController.getOtp)
 router.get("/cowin/getbydistrict", CowinController.getBydistrict)
 router.get("/cowin/wheatheroflondon", CowinController.temperatureofLondon)
+router.get("/cowin/getsortedofcities", CowinController.getsortedofcities)
+router.post("/cowin/memehandler", CowinController.meme)
 
 // WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
 
